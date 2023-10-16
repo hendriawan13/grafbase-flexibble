867ff8b11574116832073de8d28fcce371193964
Hendriawan Yudhistira <hendriawany@gmail.com>
2023-10-16 16:34:02 +0700

    update session auth

--- lib/session.ts
+++ lib/session.ts
@@ -6,20 +6,47 @@ import GoogleProvider from 'next-auth/providers/google'
 
 import jsonwebtoken from 'jsonwebtoken'
 import { JWT } from 'next-auth/jwt'
+import { SessionInterface } from "@/common.types";
 
 export const authOptions: NextAuthOptions = {
     providers: [
         GoogleProvider({
-            clientId: '',
-            clientSecret: ''
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         })
     ],
     // jwt: {
-    //     encode: () => {
+    //     encode: ({ secret, token }) => {
 
     //     },
-    //     decode: () => {
+    //     decode: async ({ secret, token }) => {
 
     //     }
-    // }
+    // },
+    theme: {
+        colorScheme: 'light',
+        logo: '/logo.png'
+    },
+    callbacks: {
+        async session({ session }) {
+            return session;
+        },
+        async signIn({ user }: { user: AdapterUser | User }) {
+            try {
+
+                
+
+                return true
+            } catch (error : any) {
+                console.log(error)
+                return false;
+            }
+        }
+    }
+}
+
+export async function getCurrentUser() {
+    const session = await getServerSession(authOptions) as SessionInterface;
+
+    return session;
 }
